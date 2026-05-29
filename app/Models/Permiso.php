ea738824a7a89f5c4c34112edcdf5ac6a731ecb0
Alexandra <ale.gave2004@gmail.com>
2026-05-29 09:02:12 -0500

    modelos Rol, Permiso y seeder de roles/permisos iniciales

--- app/Models/Permiso.php
+++ app/Models/Permiso.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permiso extends Model
 {
-    //
+    protected $connection = 'mysql';
+    protected $table      = 'permiso';
+    protected $primaryKey = 'id_permiso';
+    public $timestamps    = false;
+
+    protected $fillable = [
+        'nombre',
+        'modulo',
+    ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(
+            Rol::class,
+            'rol_permiso',
+            'id_permiso',
+            'id_rol'
+        );
+    }
 }
--- app/Models/Rol.php
+++ app/Models/Rol.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rol extends Model
 {
-    //
+    protected $connection = 'mysql';
+    protected $table      = 'rol';
+    protected $primaryKey = 'id_rol';
+    public $timestamps    = false;
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+    ];
+
+    public function permisos()
+    {
+        return $this->belongsToMany(
+            Permiso::class,
+            'rol_permiso',
+            'id_rol',
+            'id_permiso'
+        );
+    }
+
+    public function usuarios()
+    {
+        return $this->belongsToMany(
+            User::class,
+            'usuario_rol',
+            'id_rol',
+            'id_usuario'
+        );
+    }
 }
